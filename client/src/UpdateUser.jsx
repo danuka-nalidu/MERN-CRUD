a3a30b421fd9737d9342a692df72c99c8513bffd
danuka-nalidu <danukanalidu2002@gmail.com>
2025-03-16 10:46:39 +0530

    Enhance UpdateUser component with addng styles

--- client/src/UpdateUser.jsx
+++ client/src/UpdateUser.jsx
@@ -2,10 +2,31 @@ import React from "react";
 
 function UpdateUser() {
     return (
-        <div>
-            Update Users
+        <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
+            <div className="w-50 bg-white rounded p-3 shadow">
+                <form>
+                    <h2 className="text-center mb-4">Update User</h2>
+
+                    <div className="mb-3">
+                        <label className="form-label">Name</label>
+                        <input type="text" placeholder="Enter Name" className="form-control"/>
+                    </div>
+
+                    <div className="mb-3">
+                        <label className="form-label">Email</label>
+                        <input type="email" placeholder="Enter Email" className="form-control"/>
+                    </div>
+
+                    <div className="mb-3">
+                        <label className="form-label">Age</label>
+                        <input type="text" placeholder="Enter Age" className="form-control"/>
+                    </div>
+
+                    <button className="btn btn-success w-100">Update</button>
+                </form>
+            </div>
         </div>
-    )
+    );
 }
 
 export default UpdateUser;
