e73b700dff32a5fa17a03a4188245bdbe67dd23b
danuka-nalidu <danukanalidu2002@gmail.com>
2025-03-16 10:31:42 +0530

    Refactor Users component to display user data in a table format

--- client/src/Users.jsx
+++ client/src/Users.jsx
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Users() {
+    const [users, setUsers] = useState([
+        {
+            Name: "Danuka",
+            Email: "danukanalidu2002@gmail.com",
+            Age: 22,
+        },
+    ]);
     return (
-        <div>
-            Users
+        <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
+            <div className="w-50 bg-white rounded p-3">
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Age</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {users.map((user) => {
+                            return <tr>
+                                <td>{user.Name}</td>
+                                <td>{user.Email}</td>
+                                <td>{user.Age}</td>
+                                <td>
+                                    <button>Edit</button>
+                                    <button>Delete</button>
+                                </td>
+                            </tr>;
+                        })}
+                    </tbody>
+                </table>
+            </div>
         </div>
-    )
+    );
 }
 
 export default Users;
