342446804fb124d2c37aed06b30d8d5a72e89e20
danuka-nalidu <danukanalidu2002@gmail.com>
2025-07-08 19:25:31 +0530

    Implement database connection logic

--- server/index.js
+++ server/index.js
@@ -1,11 +1,27 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const UserModel = require("./models/Users");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+mongoose
+  .connect("mongodb://127.0.0.1:27017/MERN_CRUD")
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
+
+app.post("/createUser", (req, res) => {
+  UserModel.create(req.body)
+    .then((users) => res.json(users))
+    .catch((err) => res.json(err));
+});
+
 app.listen(3001, () => {
-    console.log('Server is running on port 3001');
+  console.log("Server is running on port 3001");
 });
