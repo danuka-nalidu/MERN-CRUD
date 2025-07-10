ac398d9757653baeb4c09597afd9b00cbc322e64
danuka-nalidu <danukanalidu2002@gmail.com>
2025-07-10 23:32:51 +0530

    Implement user update functionality

--- client/src/UpdateUser.jsx
+++ client/src/UpdateUser.jsx
@@ -1,32 +1,78 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
 
 function UpdateUser() {
-    return (
-        <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
-            <div className="w-50 bg-white rounded p-3 shadow">
-                <form>
-                    <h2 className="text-center mb-4">Update User</h2>
+  const { id } = useParams();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [age, setAge] = useState("");
+  const navigate = useNavigate();
 
-                    <div className="mb-3">
-                        <label className="form-label">Name</label>
-                        <input type="text" placeholder="Enter Name" className="form-control"/>
-                    </div>
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/getUser/" + id)
+      .then((result) => {
+        console.log(result);
+        setName(result.data.name);
+        setEmail(result.data.email);
+        setAge(result.data.age);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
-                    <div className="mb-3">
-                        <label className="form-label">Email</label>
-                        <input type="email" placeholder="Enter Email" className="form-control"/>
-                    </div>
+  const Update = (e) => {
+    e.preventDefault();
+    axios
+      .put("http://localhost:3001/updateUser/" + id, { name, email, age })
+      .then((result) => {
+        console.log(result);
+        navigate("/");
+      })
+      .catch((err) => console.log(err));
+  };
 
-                    <div className="mb-3">
-                        <label className="form-label">Age</label>
-                        <input type="text" placeholder="Enter Age" className="form-control"/>
-                    </div>
-
-                    <button className="btn btn-success w-100">Update</button>
-                </form>
-            </div>
-        </div>
-    );
+  return (
+    <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
+      <div className="w-50 bg-white rounded p-3 shadow">
+        <form onSubmit={Update}>
+          <h2 className="text-center mb-4">Update User</h2>
+          <div className="mb-3">
+            <label className="form-label">Name</label>
+            <input
+              type="text"
+              placeholder="Enter Name"
+              className="form-control"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Email</label>
+            <input
+              type="email"
+              placeholder="Enter Email"
+              className="form-control"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Age</label>
+            <input
+              type="text"
+              placeholder="Enter Age"
+              className="form-control"
+              value={age}
+              onChange={(e) => setAge(e.target.value)}
+            />
+          </div>
+          <button className="btn btn-success w-100">Update</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default UpdateUser;
--- client/src/Users.jsx
+++ client/src/Users.jsx
@@ -9,7 +9,7 @@ function Users() {
   useEffect(() => {
     axios
       .get("http://localhost:3001")
-      .then((result) => setUsers(result.data))
+      .then((result) => setUsers(result.data)) 
       .catch((err) => console.log(err));
   }, []);
 
@@ -36,7 +36,7 @@ function Users() {
                   <td>{user.email}</td>
                   <td>{user.age}</td>
                   <td>
-                    <Link to="/update" className="btn btn-success">
+                    <Link to={`/update/${user._id}`} className="btn btn-success">
                       Update
                     </Link>
                     <button className="btn btn-danger">Delete</button>
